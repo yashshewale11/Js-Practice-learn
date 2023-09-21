b4187f4192d5a5dbc648d8145c34cf486914d46b
yashshewale11 <127110730+yashshewale11@users.noreply.github.com>
2023-09-21 10:59:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=883014
+let accountEmail="yash@gmail.com"
+var accuntPassword="1234"
+accountCity="Nashik"
+let accountState;
+
+
+// accountId =33// not allowed
+
+accountEmail="hdfc@gmail.com"
+accuntPassword="0101"
+accountCity="Pune"
+console.log(accountId);
+
+/*
+preferd not to use var
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId,accountEmail,accuntPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) //we are using nodejs, not browser
+
+
+console.log(3+
+    3)// code readability should be high
+
+    console.log("Yash")
+
+
+    let name="Yashkumar"
+    let age=25
+    let isLoggedIn= false
+    let state;
+
+    //number => 2 to power 53
+    //bigint
+    //String => ""
+    //boolean => true/false
+    //null => standlone value
+    //undefined =>
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof age);
