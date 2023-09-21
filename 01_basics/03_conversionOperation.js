c4cc78dd51abafb94d5d0ec884f8512982010fae
yashshewale11 <127110730+yashshewale11@users.noreply.github.com>
2023-09-21 13:04:12 +0000

    d

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="Yashkumar"
+
+//console.log(typeof score);
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" =>NaN
+//true => 1; false =>0
+
+let isLoggedIn= 1
+
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//1 => true; 0 => false
+//" " => false;
+//"Yash" => true;
+
+let someNumber =33
+
+let StringNumber = String(someNumber)
+
+console.log(StringNumber);
+console.log(typeof StringNumber);
+ 
