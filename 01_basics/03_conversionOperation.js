092e2815200f52fddd4dd92192e7c034733f8aa0
yashshewale11 <127110730+yashshewale11@users.noreply.github.com>
2023-09-22 06:27:33 +0000

    comparison clean code

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,6 +23,41 @@ let someNumber =33
 
 let StringNumber = String(someNumber)
 
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+//************************Operations *************************/
+
+//  let value=3
+//  let negValue= -value
+//  console.log(negValue);
+
+
+//  console.log(2+2);
+
+ let str1="Hello"
+ let str2=" Yash"
+
+ let str3=str1+str2
+//  console.log(str3);
+
+//  console.log
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log(1+2+"2");
+
+
+//  console.log()
+
+
+//  let num1,num2,num3
+
+//  num1=num2=num3=3=2+2
  
+ let gameCounter=100
+ gameCounter++
+ console.log(gameCounter);
+
+ //link to study
+ //https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
