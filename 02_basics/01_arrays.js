a6750b5a28d1d8a161c0ff16d4ac170d0af4a35c
yashshewale11 <127110730+yashshewale11@users.noreply.github.com>
2023-09-25 04:45:08 +0000

    Arrays in JS

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+ 
+const myArr=[0,1,2,3,4,5]
+
+// const myHeros=["Bhagat Singh","Shivaji Maharaj"]
+
+// const myArr2= new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+//myArr.unshift(8)
+//myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// const newArr=myArr.join()
+
+ console.log(myArr);
+// console.log(typeof newArr);
+
+
+//Slice ,Splice
+
+
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
+
+
+
