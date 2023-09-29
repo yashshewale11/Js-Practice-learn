c766453df96eac061c4c4c1c1185585394a8cdc5
yashshewale11 <127110730+yashshewale11@users.noreply.github.com>
2023-09-29 04:01:35 +0000

    new changes

--- 07_Project_01/colorChange.js
+++ 07_Project_01/colorChange.js
@@ -4,11 +4,12 @@ const body = document.querySelector('body');
 
 buttons.forEach(function (button) {
   console.log(button);
-  button.addEventListener('click', function (e) {
+  button.addEventListener('mouseover', function (e) {
     console.log(e);
     console.log(e.target);
     if (e.target.id === 'grey') {
       body.style.backgroundColor = e.target.id;
+     
     }
     if (e.target.id === 'white') {
       body.style.backgroundColor = e.target.id;
